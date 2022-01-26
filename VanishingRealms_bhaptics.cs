2b9427cb6aeb9cba95a0b053da87c086b369c399
floh-bhaptics <floh@fahrenbergers.de>
2022-01-26 13:31:16 +0100

    Projektdateien hinzufügen.

---
+++ VanishingRealms_bhaptics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MelonLoader;
+using HarmonyLib;
+
+using MyBhapticsTactsuit;
+
+namespace VanishingRealms_bhaptics
+{
+    public class VanishingRealms_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        /*
+        [HarmonyPatch(typeof(VertigoPlayer), "Die", new Type[] { })]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
+
+    }
+}
